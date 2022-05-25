82a02dd1bf7717c801fee947daf0a33dd040e376
RelinPramudiya <relinrp@mhs.mdp.ac.id>
2022-05-25 21:31:56 +0700

    commit
    part 1

---
+++ app/src/main/java/com/if4b/aplikasi_mesjid/ModelMesjid.java
@@ -0,0 +1,31 @@
+package com.if4b.aplikasi_mesjid;
+
+public class ModelMesjid {
+    private String nama;
+    private String tentang;
+    private String foto;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String poto) {
+        this.foto = foto;
+    }
+}
