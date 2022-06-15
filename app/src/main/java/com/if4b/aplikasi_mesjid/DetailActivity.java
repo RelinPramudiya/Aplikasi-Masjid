d384982c607e2970d46b9e7e123793d63e9834c0
RelinPramudiya <relinrp@mhs.mdp.ac.id>
2022-06-15 10:38:07 +0700

    commit

--- app/src/main/java/com/if4b/aplikasi_mesjid/DetailActivity.java
+++ app/src/main/java/com/if4b/aplikasi_mesjid/DetailActivity.java
@@ -12,21 +12,19 @@ import com.bumptech.glide.Glide;
 public class DetailActivity extends AppCompatActivity {
     private TextView tvNama,tvTentang;
     private ImageView ivFoto;
-
+    private String yNama, yTentang, yFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ivFoto = findViewById(R.id.iv_foto);
-        tvNama = findViewById(R.id.tv_nama_masjid);
-        tvTentang = findViewById(R.id.tv_tentang_masjid);
+        initView();
 
         Intent terima = getIntent();
-        String yNama = terima.getStringExtra("xNama");
-        String yTentang = terima.getStringExtra("xTentang");
-        String yFoto = terima.getStringExtra("xFoto");
+        yNama = terima.getStringExtra("xNama");
+        yTentang = terima.getStringExtra("xTentang");
+        yFoto = terima.getStringExtra("xFoto");
 
         getSupportActionBar().setTitle(yNama);
 
@@ -35,6 +33,11 @@ public class DetailActivity extends AppCompatActivity {
         Glide.with(DetailActivity.this)
                 .load(yFoto)
                 .into(ivFoto);
+    }
+    private void initView(){
+        ivFoto = findViewById(R.id.iv_foto);
+        tvNama = findViewById(R.id.tv_nama_masjid);
+        tvTentang = findViewById(R.id.tv_tentang_masjid);
     }
 
 }
